4af7e8ae8f07f643b02c4e3dca0a3f3fdca7ef3c
MrSuperTop <60810876+MrSuperTop@users.noreply.github.com>
2020-07-02 14:42:40 +0300

    Dark theme add

--- Easy_DD/js/MyScripts.js
+++ Easy_DD/js/MyScripts.js
@@ -3,6 +3,16 @@ let allBodies = document.querySelectorAll ('.collabsible-body');
 let allLies = document.querySelectorAll ('.collabsible > li');
 let bodyiesHeights = [];
 
+//
+
+function cssVar (name, value) {
+  if (name[0] != '-') name = '--' +name
+  if (value) document.documentElement.style.setProperty (name, value)
+  return getComputedStyle (document.documentElement).getPropertyValue (name);
+}
+
+//
+
 for (let body of allBodies) {
 	body.style.height = 'auto';
 	bodyiesHeights.push (body.offsetHeight)
@@ -41,3 +51,17 @@ for (let header of allHeaders) {
 		}
 	})
 }
+
+let theme = 'white'
+
+document.querySelector ('.theme-changer-box').addEventListener ('click', function () {
+	if (theme == 'white') {
+		cssVar ('main-color', '#1e1e24');
+		cssVar ('font-color', '#fff');
+		theme = 'black'
+	} else {
+		cssVar ('main-color', '#fff');
+		cssVar ('font-color', '#000');
+		theme = 'white'
+	}
+})
