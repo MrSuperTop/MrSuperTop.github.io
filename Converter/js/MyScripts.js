58dabb14a8bd0b201a7f798b0a009e6d05284cbe
MrSuperTop <60810876+MrSuperTop@users.noreply.github.com>
2020-07-18 13:23:15 +0300

    New converter

--- Converter/js/MyScripts.js
+++ Converter/js/MyScripts.js
@@ -51,7 +51,7 @@ startBtn.addEventListener ('click', function () {
 var toReversTextArea = document.querySelector ('.to-revers-text');
 document.querySelector ('.result-reversed').hidden = true;
 
-toReversTextArea.addEventListener ('keyup', function () {
+toReversTextArea.addEventListener ('input', function () {
 	splitedString = document.querySelector ('.to-revers-text').value.split ('');
 
 	let reversed = '';
@@ -91,5 +91,78 @@ document.querySelector ('.variation-start-btn').addEventListener ('click', funct
 		variatedResElement.innerHTML = 'Result: ' + variatedString;
 	} else {
 		variatedResElement.hidden = true;
+	}
+})
+
+function charInfo (char) {
+	let charCode = char.charCodeAt (0);
+
+	if (charCode >= 97 && charCode <= 122) {
+		return ['lowerCase', charCode];
+	} else if (charCode >= 65 && charCode <= 90) {
+		return ['upperCase', charCode];
+	} else {
+		return ['notALetter', charCode];
+	}
+}
+
+function isItANumber (char) {
+	let charCode = char.charCodeAt (0);
+	if (charCode >= 47 && charCode <= 57) return true;
+	else return false;
+}
+
+let toCodeTextArea = document.querySelector ('.to-code-text');
+let shiftInput = document.querySelector ('.shift');
+document.querySelector ('.result-coded').hidden = true;
+
+toCodeTextArea.addEventListener ('input', function () {
+	let toWorkWith = toCodeTextArea.value.split ('');
+	let numberToDecode = [];
+
+	for (let char of toWorkWith.slice (0, 3)) {
+		if (isItANumber (char) || char == '-') numberToDecode.push (char)
+	}
+
+	if (numberToDecode.length != 0) numberToDecode = +numberToDecode.join ('');
+
+
+	let result = [];
+	let toPushCode;
+	let infoResult;
+	let shift;
+
+	for (let char of toWorkWith) {
+		infoResult = charInfo (char)
+
+		if (infoResult [0] != 'notALetter') {
+			numberToDecode.length == 0 ? shift = +shiftInput.value : shift = numberToDecode * -1;
+			toPushCode = char.charCodeAt (0) + shift;
+			infoResult [1] += shift;
+
+			if (infoResult [0] == 'lowerCase' && infoResult [1] < 97) toPushCode += 26;
+			if (infoResult [0] == 'lowerCase' && infoResult [1] > 122) toPushCode -= 26;
+			if (infoResult [0] == 'upperCase' && infoResult [1] < 65) toPushCode += 26;
+			if (infoResult [0] == 'upperCase' && infoResult [1] > 90) toPushCode -= 26;
+
+			result.push (String.fromCharCode (toPushCode));
+		} else {
+			result.push (char)
+		}
+	}
+
+	let codedResElement = document.querySelector ('.result-coded');
+
+	if (result.join != 'undefined' && result.join ('').length != 0) {
+		codedResElement.hidden = false;
+		if (numberToDecode.length == 0) codedResElement.innerHTML = 'Result: ' + +shiftInput.value + result.join ('');
+		else {
+			for (let item of result.slice (0, 3)) {
+				if (isItANumber (item) || item == '-') delete result [result.indexOf (item)]
+			}
+			codedResElement.innerHTML = result.join ('');
+		}
+	} else {
+		codedResElement.hidden = true;
 	}
 })
