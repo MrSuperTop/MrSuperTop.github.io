d44a6a67a3ef0d787119608bbda8dbf1e09667d3
Evgeniy <evegeniy.konyshev2007@gmail.com>
2020-08-11 10:55:56 +0300

    Password checker added

---
+++ PasswordLevelChecker/js/script.js
@@ -0,0 +1,128 @@
+// Css vars func
+
+function cssVar (name, value) {
+  if (name [0] != '-') name = '--' +name
+  if (value) document.documentElement.style.setProperty (name, value)
+  return getComputedStyle (document.documentElement).getPropertyValue (name);
+}
+
+let transition = cssVar ('transition-primary');
+transition = +transition.slice (0, transition.length - 1) * 1000;
+let halfTransition = transition / 2;
+
+// Resizeble button
+
+let allPressebleButtons = document.querySelectorAll ('.presseble-btn');
+
+function resizeButton (event) {
+	let target = event.target.closest ('div');
+
+	target.classList.add ('small-btn')
+	setTimeout (() => target.classList.remove ('small-btn'), transition / 2)
+}
+
+for (let button of allPressebleButtons) {
+	button.addEventListener ('click', resizeButton)
+}
+
+// theme toggler
+
+let themeToggler = document.querySelector ('.theme-toggler');
+let prevValue;
+
+function changeIcon (iconName) {
+	setTimeout (() => {
+		themeToggler.firstChild.style.opacity = '0';
+		themeToggler.firstChild.innerHTML = iconName;
+		themeToggler.firstChild.style.opacity = '1';
+	}, transition / 3);
+}
+
+function setTheme (theme) {
+	if (theme == 'dark') {
+		cssVar ('bgc-primary', '#1e1e24');
+		cssVar ('bgc-secondary', '#ddd');
+
+		cssVar ('fc-primary', '#fff');
+		cssVar ('fc-secondary', '#000');
+
+		changeIcon ('nights_stay');
+	} else if (theme == 'ligth') {
+		cssVar ('bgc-primary', '#ddd');
+		cssVar ('bgc-secondary', '#1e1e24');
+
+		cssVar ('fc-primary', '#000');
+		cssVar ('fc-secondary', '#fff');
+
+		changeIcon ('wb_sunny');
+	}
+}
+
+prevValue = cssVar ('transition-primary');
+cssVar ('transition-primary', '0s');
+
+localStorage.theme == 'dark' ? setTheme ('dark') : setTheme ('ligth');
+
+cssVar ('transition-primary', prevValue);
+
+
+function toggleTheme () {
+
+	cssVar ('bgc-primary') == '#ddd' ? localStorage.theme = 'dark' : localStorage.theme = 'ligth';
+
+	if (cssVar ('bgc-primary') == '#ddd' || cssVar ('bgc-primary') == '#ddd') setTheme ('dark');
+	else setTheme ('ligth');
+}
+
+themeToggler.addEventListener ('click', toggleTheme);
+
+//
+
+let allCheckers = document.querySelectorAll ('.checker');
+let passwordInput = document.querySelector ('.password-to-check');
+
+let charsObjc = {
+	lowercase: generateCharsArray (97, 26),
+	uppercase: generateCharsArray (65, 26),
+	numbers: generateCharsArray (48, 10),
+	specialSymbols: [...generateCharsArray (33, 15), ...generateCharsArray (58, 7), ...generateCharsArray (123, 4)],
+};
+
+function generateCharsArray (start, howManyChars) {
+	let result = [];
+	for (let i = 0; i < howManyChars; i++) {
+		result.push (String.fromCharCode (i + start))
+	}
+	return result
+}
+
+function addOk (checker) {
+	checker.classList.remove ('bad');
+	checker.classList.add ('ok');
+	checker.firstChild.classList.remove ('fa-exclamation')
+	checker.firstChild.classList.add ('fa-check')
+}
+
+function addBad (checker) {
+	checker.classList.remove ('ok');
+	checker.classList.add ('bad');
+	checker.firstChild.classList.add ('fa-exclamation')
+	checker.firstChild.classList.remove ('fa-check')
+}
+
+function hasLetter (word, whichLetter) {
+	for (let char of word) {
+		if (whichLetter == 'Uppercase' && charsObjc.uppercase.includes (char)) return true;
+		if (whichLetter == 'Lowercase' && charsObjc.lowercase.includes (char)) return true;
+		if (whichLetter == 'Number' && charsObjc.numbers.includes (char)) return true;
+		if (whichLetter == 'SpecialCaracter' && charsObjc.specialSymbols.includes (char)) return true;
+	}
+}
+
+passwordInput.addEventListener ('input', () => {
+	passwordInput.value.length >= 8 ? addOk (allCheckers [0]) : addBad (allCheckers [0]);
+	hasLetter (passwordInput.value, 'Uppercase') ? addOk (allCheckers [1]) : addBad (allCheckers [1]);
+	hasLetter (passwordInput.value, 'Lowercase') ? addOk (allCheckers [2]) : addBad (allCheckers [2]);
+	hasLetter (passwordInput.value, 'Number') ? addOk (allCheckers [3]) : addBad (allCheckers [3]);
+	hasLetter (passwordInput.value, 'SpecialCaracter') ? addOk (allCheckers [4]) : addBad (allCheckers [4]);
+})
